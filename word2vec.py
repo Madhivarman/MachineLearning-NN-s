485a7c49be949393ab16ed274acffb407ba5622b
Madhivarman <madhivarman451@gmail.com>
2018-05-25 15:35:25 +0530

    Update word2vec.py

--- word2vec.py
+++ word2vec.py
@@ -12,6 +12,12 @@ import numpy as np
 import tensorflow as tf 
 
 
+data_index = 0 #global variable
+
+valid_size = 16
+valid_window = 100
+num_sampled = 64 #number of negative samples to sample
+
 def download_the_dataset(filename,url,expected_bytes):
 
 	print("Downloading the file..Wait for a seconds")
@@ -84,7 +90,46 @@ def build_dataset(words,n_words):
 
 	return data, count, dictionary,reverse_dictionary
 
-def run(graph,num_steps,data,batch_size,num_skips,skip_wndw):
+
+def generate_batch(data,batch_size,number_skip,skip_window):
+	global data_index
+	assert batch_size % number_skip == 0
+	assert number_skip <= 2 * skip_window
+
+	batch = np.ndarray(shape=(batch_size),dtype=np.int32)
+	context = np.ndarray(shape=(batch_size,1),dtype=np.int32)
+	span = 2*skip_window + 1 #[skip_window,input_window,skip_window]
+
+	buffers = collections.deque(maxlen=span)
+
+	for _ in range(span):
+		buffers.append(data[data_index])
+		data_index = (data_index + 1) % len(data)
+
+	for i in range(batch_size // number_skip):
+		target = skip_window #input word at center of the buffer
+		targets_to_avoid = [skip_window]
+
+		for j in range(number_skip):
+
+			while target in targets_to_avoid:
+				target = random.randint(0, span-1)
+
+			targets_to_avoid.append(target)
+			batch[i * number_skip + j] = buffers[skip_window] #input word
+			context[i *  number_skip + j,0] = buffers[target] #context words
+
+		buffers.append(data[data_index])
+		data_index = (data_index + 1) % len(data)
+
+	#backtrack a little bit to avoid skipping words in end of the batch
+	data_index = (data_index + len(data) - span) % len(data)
+
+	return batch,context
+
+
+
+def run(init,graph,num_steps,data,batch_size,num_skips,skip_wndw,train_inputs,train_context,optimizer,cross_entropy,similarity,valid_examples,normalized_embeddings,reverse_dictionary):
 
 	with tf.Session(graph=graph) as session:
 
@@ -92,7 +137,7 @@ def run(graph,num_steps,data,batch_size,num_skips,skip_wndw):
 		init.run()
 		print("Initialized All Variables")
 
-		avg_loss = 0 #initially
+		average_loss = 0 #initially
 
 		for step in range(num_steps):
 
@@ -101,7 +146,34 @@ def run(graph,num_steps,data,batch_size,num_skips,skip_wndw):
 
 			feed_dict = {train_inputs:batch_inputs,train_context:batch_context}
 
-			
+			# we perform one update step by step evaluating the optimizer op
+			_,loss_val = session.run([optimizer,cross_entropy],feed_dict=feed_dict)
+			average_loss += loss_val
+
+			if step % 2000 == 0:
+				if step > 0:
+					average_loss /= 2000
+
+				#average loss is an estimate of the loss over last 2000 batches
+				print("Average loss at step:{s},average loss:{avg}".format(s=step,avg=average_loss))
+				average_loss = 0 #reinitialize
+
+			if step % 10000 == 0:
+				sim = similarity.eval()
+				for i in range(valid_size):
+					valid_word = reverse_dictionary[valid_examples[i]]
+					top_k = 8 #top 8 words
+					nearest = (-sim[i, :]).argsort()[1:top_k+1]
+					log_str = "Nearest to {original_word}:\t".format(original_word = valid_word)
+
+					for k in range(top_k):
+						close_word = reverse_dictionary[nearest[k]]
+						log_str = "{before} {after}".format(before=log_str,after=close_word)
+
+					print(log_str)
+
+		final_embeddings = normalized_embeddings.eval()
+
 
 def main():
 	#download the dataset
@@ -130,10 +202,7 @@ def main():
 	skip_wnd = 2
 	num_skips = 2 #how many times to reuse an input to generate a  model
 
-	valid_size = 16
-	valid_window = 100
 	valid_examples = np.random.choice(valid_window,valid_size,replace = False) #randomly select 16 index numbers within 100.
-	num_sampled = 64 #number of negative samples to sample
 
 	graph = tf.Graph()
 
@@ -150,11 +219,12 @@ def main():
 		embed = tf.nn.embedding_lookup(embeddings,train_inputs)
 
 		#softmax variables
-		weights = tf.Variable(tf.truncated_normal([vocabulary_size,embedding_size],
-			stddev=1.0/math.sqrt(embedding_size))) #(10000,300)
+		weights = tf.Variable(
+			tf.truncated_normal([embedding_size,vocabulary_size],
+				stddev=1.0/math.sqrt(embedding_size)))
 
 		biases = tf.Variable(tf.zeros([vocabulary_size])) #(10000)
-		hidden_out = tf.transpose(tf.matmul(embed, tf.transpose(weights))) + biases
+		hidden_out = tf.transpose(tf.matmul(tf.transpose(weights),tf.transpose(embed))) + biases
 
 		#convert train_context to a one-hot format
 		train_one_hot = tf.one_hot(train_context,vocabulary_size)
@@ -162,7 +232,7 @@ def main():
 						tf.nn.softmax_cross_entropy_with_logits(logits=hidden_out,labels=train_one_hot)
 						)
 		#opitmizer
-		optimizer = tf.train.GradientDescentOptimizer(0.1).minimize(cross_entropy)
+		optimizer = tf.train.GradientDescentOptimizer(1.0).minimize(cross_entropy)
 
 		#compute the cosine similarity between minibatch examples and all embeddings.
 		#L2 normalization is used
@@ -182,6 +252,16 @@ def main():
 
 		#add variable initializer
 		init = tf.global_variables_initializer()
+
+
+	#paramaters
+	num_steps = 50000
+	softmax_start_time = dt.datetime.now()
+	run(init,graph,num_steps,data,batch_size,num_skips,skip_wnd,train_inputs,train_context,optimizer,cross_entropy,similarity,valid_examples,normalized_embeddings,reverse_dictionary)
+	softmax_end_time = dt.datetime.now()
+
+	print("Softmax method took {} minutes to run 50000 iterations".format((softmax_end_time - softmax_start_time).total_seconds()))
+
 
 if __name__ == '__main__':
 	main()
